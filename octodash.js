1bb0af65713a9333fd6dab64a3a9920c3f4a4e31
Steve Fenton <steve.fenton@octopus.com>
2022-02-07 15:49:30 +0000

    Initial extension for version highlighting

---
+++ octodash.js
@@ -0,0 +1,41 @@
+const colouring = function () {
+    var colors = ['#fea3aa', '#f8b88b', '#faf884', '#baed91', '#b2cefe', '#f2a2e8'];
+    var nextColorIndex = 0;
+    var versions = [];
+
+    var elems = document.querySelectorAll('[class^=style_version__');
+    console.log(`Octodash found ${elems.length} items`);
+
+    for (var i = 0; i < elems.length; i++) {
+        var version = elems[i].innerHTML;
+        var color = null;
+        for (var j = 0; j < versions.length; j++) {
+            if (versions[j].version == version) {
+                color = versions[j].color;
+                break;
+            }
+        }
+        
+        if (!color) {
+            color =  colors[nextColorIndex];
+            versions.push({
+                version: version,
+                color: color
+            });
+            
+            nextColorIndex++;
+            if (nextColorIndex >= colors.length) {
+                nextColorIndex = 0;
+            }
+        }
+
+        elems[i].parentElement.parentElement.parentElement.style.backgroundColor = color;
+    }
+};
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: colouring
+    });
+  });
